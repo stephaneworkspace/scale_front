662f605ad505feae736a465a8a165b03867449d1
stephaneworkspace <s.bressani@bluewin.ch>
2019-12-21 15:10:57 +0100

    Adding Rust in XCode - Swift
    OSX

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -14,6 +14,7 @@ import Flutter
     CHANNEL.setMethodCallHandler { /*[unowned self]*/ (methodCall, result) in
         if methodCall.method == "Printy" {
             result("Hi from Swift")
+            hello_world()
         }
     }
     // En call swift
